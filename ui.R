4ff544efe8b744f7acb1d38b6734b11bb8d6c661
s1nzy <sanziana.cristea@gmail.com>
2019-01-20 18:03:27 +0100

    final ui
    to match amika published model

--- ui.R
+++ ui.R
@@ -21,39 +21,40 @@ shinyUI(fluidPage(
       h4("Dosing details"),
       hr(),
       selectInput('admin', label='Route of administration', c("I.V. infusion","I.V. bolus")),
-      numericInput("dose", label='Dose', value = 10,min=0),
+      numericInput("dose", label='Dose (mg/kg)', value = 12,min=0),
+      numericInput("d2", label='Infusion duration (min)', value = 20,min=0),
+      numericInput("ii1", label='Dosing interval (hours)', value = 24,min=0),
+      
       
       br(),
       h4("Simulation details"),
       hr(),
       numericInput("nsim", label='Number of samples for simulation', value = 1000,min=0),
-      numericInput("simtime", label='Simulation time', value = 12,min=0),
+      numericInput("simtime", label='Simulation time', value = 125,min=0),
       numericInput("minprob", label='Minimal probability', value = 0.25,min=0,max=1),
       numericInput("maxprob", label='Maximal probability', value = 0.75,min=0,max=1),
       
       br(),
       h4("Patient covariates"),
       hr(),
-      numericInput("bw", label='Birth weight (g)', value = 1500,min=0),
-      numericInput("cw", label='Current weight (g)', value = 1500,min=0),
-      numericInput("pna", label='Postnatal age (days)', value = 2,min=0),
-      selectInput("select", h4("Ibuprofen co-administration?"), 
-                  choices = list("Yes" = 1, "No" = 0), selected = 0), # to be changed to multiple choice as it can only be 0 or 1
-      
+      numericInput("bw", label='Birth weight (g)', value = 1730,min=0),
+      numericInput("cw", label='Current weight (g)', value = 2800,min=0),
+      numericInput("pna", label='Postnatal age (days)', value = 5,min=0),
+      selectInput("nsaid", h4("Ibuprofen co-administration?"), 
+                  choices = list("Yes" = 1, "No" = 0), selected = 0), 
       br(),
       h4("IIV"),
       hr(),
-      #numericInput("etaka", label='ETA - Absorption rate constant', value = 10,min=0),
-      numericInput("etav1", label='ETA - Central volume', value = 10,min=0),
-      numericInput("etav2", label='ETA - Peripheral volume', value = 10,min=0),
-      numericInput("etaq1", label='ETA - Inter-compartmental clearance', value = 10,min=0),
-      numericInput("etacl", label='ETA - Clearance', value = 10,min=0),
+      numericInput("etav1", label='ETA - Central volume', value = 0.09,min=0),
+      numericInput("etav2", label='ETA - Peripheral volume', value = 0,min=0),
+      numericInput("etaq1", label='ETA - Inter-compartmental clearance', value = 0,min=0),
+      numericInput("etacl", label='ETA - Clearance', value = 0,min=0),
       
       br(),
       h4("Residual variability"),
       hr(),
-      numericInput("sigmaprop", label='Proportional', value = 0.01,min=0),
-      numericInput("sigmaadd", label='Additive', value = 0.1,min=0)
+      numericInput("sigmaprop", label='Proportional', value = 0.0614,min=0),
+      numericInput("sigmaadd", label='Additive', value = 0.267,min=0)
       
     ),
 
